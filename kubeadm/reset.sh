4f3f3c132aa6895e416ef3eb68ada4aa204cc9eb
jaehoon <jaehoon.jung@kubeworks.net>
2024-12-30 20:15:55 +0900

    update: reset script

--- kubeadm/reset.sh
+++ kubeadm/reset.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-for node in node-01 node-02 node-03
+for node in node-01 node-02 
 do
   echo "==== kubeadm reset ===="
-  ssh $node kubeadm reset --force
+  ssh -i ../idrsa root@$node kubeadm reset --force
   echo "==== clean cni plugins & iptables ===="
-  ssh $node rm -rf kubeadm && rm -rf /etc/cni/net.d 
+  ssh -i ../idrsa root@$node rm -rf kubeadm && rm -rf /etc/cni/net.d 
 done
